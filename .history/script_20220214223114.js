03a134dc62370731f65616c4f854c8679d79da9c
ArusyakAghajanyan <ayad_aga@mail.ru>
2022-02-15 01:33:30 +0400

    first commit

---
+++ .history/script_20220214223114.js
@@ -0,0 +1,20 @@
+const leftButton = document.querySelector(".leftButton");
+const centerButton = document.querySelector(".centerButton");
+const rightButton = document.querySelector(".rightButton");
+const num = document.getElementById("num");
+
+let i = 0;
+leftButton.addEventListener("click", function minus() {
+    i--;
+    num.innerHTML = i;
+});
+
+rightButton.addEventListener("click", function pulus() {
+    i++;
+    num.innerHTML = i;
+});
+
+centerButton.addEventListener("click", function reset() {
+    i = 0;
+    num.innerHTML = i;
+});
